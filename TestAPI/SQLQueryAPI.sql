25b358c8220ec6d119d032332961880981884322
CesarFidel <35857187+CesarFidel@users.noreply.github.com>
2021-10-27 19:46:32 -0500

    Agregados métodos a la api
    Se agregaron métodos post, get y delete a la api

--- TestAPI/SQLQueryAPI.sql
+++ TestAPI/SQLQueryAPI.sql
@@ -6,7 +6,7 @@ CREATE TABLE Post (
     Id int NOT NULL IDENTITY(1,1) ,
     Title varchar(255),
     Boby varchar(255),
-    PRIMARY KEY (UserId)
+    PRIMARY KEY (Id)
 );
 
 INSERT INTO Post (UserId,Title,Boby) VALUES (1,'sunt aut facere repellat provident occaecati excepturi optio reprehenderit','quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto');
@@ -14,6 +14,3 @@ INSERT INTO Post (UserId,Title,Boby) VALUES (1,'sunt aut facere repellat provide
 INSERT INTO Post (UserId,Title,Boby) VALUES (1,'qui est esse','est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla');
 
 INSERT INTO Post (UserId,Title,Boby) VALUES (2,'et ea vero quia laudantium autem','delectus reiciendis molestiae occaecati non minima eveniet qui voluptatibus\naccusamus in eum beatae sit\nvel qui neque voluptates ut commodi qui incidunt\nut animi commodi');
-
-
-DROP TABLE Post
--- TestAPI/TestAPI/Controllers/PostController.cs
+++ TestAPI/TestAPI/Controllers/PostController.cs
@@ -16,8 +16,48 @@ namespace TestAPI.Controllers
         {
             using (var db = new Models.testAPIContext())
             {
-                IEnumerable<Models.Post> posts = db.Posts.ToList();
+                var posts = db.Posts.ToList();
                 return posts;
+            }
+        }
+
+        [HttpGet("GetPostById/{id}")]
+        public async Task<Models.Post> GetPostById(int id)
+        {
+            using (var db = new Models.testAPIContext())
+            {
+                var post = await db.Posts.FindAsync(id);
+                return post;
+            }
+        }
+
+        [HttpGet("GetPostByUserId/{userId}")]
+        public IEnumerable<Models.Post> GetPostByUserId(int userId)
+        {
+            using (var db = new Models.testAPIContext())
+            {
+                var posts = db.Posts.Where(p => p.UserId == userId).ToList();
+                return posts;
+            }
+        }
+
+        [HttpPost]
+        public void AddPost(Models.Post post)
+        {
+            using (var db = new Models.testAPIContext())
+            {
+                db.Posts.Add(post);
+                db.SaveChanges();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public void DeletePostById(int id)
+        {
+            using (var db = new Models.testAPIContext())
+            {
+                db.Posts.Remove(db.Posts.Find(id));
+                db.SaveChanges();
             }
         }
     }
